d211cd44c20b712446dd311ba1c653991ca54406
Alex Birch <Birch-san@users.noreply.github.com>
2018-01-06 20:31:58 +0000

    perf

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # container_gem_cache ensures that the container need not run bundle install on every startup
 exec docker run -it --rm --init \
--v "$PWD/src":/srv/jekyll \
--v "$PWD/container_gem_cache":/usr/local/bundle \
+-v "$PWD/src":/srv/jekyll:delegated \
+-v "$PWD/container_gem_cache":/usr/local/bundle:delegated \
 -p 8087:4000 \
 my/jekyll:3.7.0

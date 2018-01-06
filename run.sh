913f6b7c5516ca2ff852692429d5c9f6cce0e88a
Alex Birch <Birch-san@users.noreply.github.com>
2018-01-06 21:11:51 +0000

    separate src and dist

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 exec docker run -it --rm --init \
 -v "$PWD/src":/srv/jekyll:delegated \
 -v "$PWD/container_gem_cache":/usr/local/bundle:delegated \
+-v "$PWD/dist":/dist \
 -p 8087:4000 \
 -p 35729:35729 \
 -p 3000:3000 \
-my/jekyll:3.7.0 jekyll serve --livereload --incremental
+my/jekyll:3.7.0 jekyll serve --livereload --incremental \
+-d /dist

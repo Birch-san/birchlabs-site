aef86ea0c8523d01581b95314e87559967da3f82
Alex Birch <Birch-san@users.noreply.github.com>
2018-06-09 12:30:09 +0100

    install jekyll-postfiles, retain knowledge of how to develop against local copy. add local dependency updater. update to Jekyll 3.8.3.

--- gem_update.sh
+++ gem_update.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 # container_gem_cache ensures that the container need not run bundle install on every startup
 exec docker run -it --rm --init \
---name jekyll_build \
+--name jekyll_gem \
 -v "$PWD/src":/srv/jekyll:delegated \
 -v "$PWD/container_gem_cache":/usr/local/bundle:delegated \
 `#-v "$HOME/git/jekyll-postfiles":/jekyll-postfiles:delegated` \
--v "$PWD/dist":/dist:delegated \
-jekyll/jekyll:3.8.3 jekyll build -d /dist
+jekyll/jekyll:3.8.3 bundle
--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ exec docker run -it --rm --init \
 --name jekyll_run \
 -v "$PWD/src":/srv/jekyll:delegated \
 -v "$PWD/container_gem_cache":/usr/local/bundle:delegated \
+`#-v "$HOME/git/jekyll-postfiles":/jekyll-postfiles:delegated` \
 -v "$PWD/dist":/dist:delegated \
 -p 8087:4000 \
 -p 35729:35729 \
 -p 3000:3000 \
-jekyll/jekyll:3.8.0 jekyll serve --livereload -d /dist
+jekyll/jekyll:3.8.3 jekyll serve --livereload -d /dist
